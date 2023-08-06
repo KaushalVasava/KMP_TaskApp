31cd78380d803978637e889f86a4d3e1512fdacd
KaushalVasava <vasavakaushal333@gmail.com>
2023-08-06 20:25:00 +0530

    Add adjustResize flag in android manifest

--- androidApp/src/main/java/com/kaushalvasava/apps/taskapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/kaushalvasava/apps/taskapp/android/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.kaushalvasava.apps.taskapp.android
 
+import TaskAppTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.kaushalvasava.apps.taskapp.MainView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainView()
+            TaskAppTheme {
+                Surface(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                    MainView()
+                }
+            }
         }
     }
 }
